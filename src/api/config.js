1cb3379ee2b875e9f177eeff9657917058062de2
Kritik Nayaju <kirtiknayaju@gmail.com>
2024-12-01 12:31:02 +0545

    render link is added

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  "https://coffee-backend-vvob.onrender.com" || "http://localhost:3001/api/v1";
+  "https://coffee-backend-vvob.onrender.com/api/v1" || "http://localhost:3001/api/v1";
 
 export const api = axios.create({
   baseURL,
